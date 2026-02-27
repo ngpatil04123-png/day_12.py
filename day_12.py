4bb35f853c8c196ae1be428be4aa018e383fa001
Navollas patil <ngpatil04123@gmail.com>
2026-02-27 23:53:52 +0530

    Refactor code to include swapping and leap year check

--- day_12.py
+++ day_12.py
@@ -1,20 +1,20 @@
-# print("Never give up !!")
-# a = int(input("Enter first number: "))
-# b = int(input("Enter second number: "))
+print("Never give up !!")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 
-# a, b = b, a
+a, b = b, a
 
-# print("After swapping:")
-# print("a =", a)
-# print("b =", b)
-#--------------------------------------------------------------------------------------------------------------------------------------
-# year = int(input("Enter the  year: "))
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+--------------------------------------------------------------------------------------------------------------------------------------
+year = int(input("Enter the  year: "))
 
-# if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-#     print("Leap Year")
-# else:
-#     print("Not a Leap Year")
-#---------------------------------------------------------------------------------------------------------------------------------------
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
+---------------------------------------------------------------------------------------------------------------------------------------
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 op = input("Enter operator (+, -, *, /): ")
@@ -28,4 +28,5 @@ elif op == "*":
 elif op == "/":
     print("Result:", a / b)
 else:
+
     print("Invalid operator")
