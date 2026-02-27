891be3b65337274529679c9b42dfce7454ea15d2
Navollas patil <ngpatil04123@gmail.com>
2026-02-27 23:52:31 +0530

    Add files via upload

---
+++ day_12.py
@@ -0,0 +1,31 @@
+# print("Never give up !!")
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# a, b = b, a
+
+# print("After swapping:")
+# print("a =", a)
+# print("b =", b)
+#--------------------------------------------------------------------------------------------------------------------------------------
+# year = int(input("Enter the  year: "))
+
+# if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+#     print("Leap Year")
+# else:
+#     print("Not a Leap Year")
+#---------------------------------------------------------------------------------------------------------------------------------------
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /): ")
+
+if op == "+":
+    print("Result:", a + b)
+elif op == "-":
+    print("Result:", a - b)
+elif op == "*":
+    print("Result:", a * b)
+elif op == "/":
+    print("Result:", a / b)
+else:
+    print("Invalid operator")
